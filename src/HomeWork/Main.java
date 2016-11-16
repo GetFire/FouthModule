8e4f30e9f149956a60b6fab798e9b9400514f143
Denis Azhymov <azhimov93@gmail.com>
2016-11-16 17:20:32 +0200

    init

--- src/HomeWork/Main.java
+++ src/HomeWork/Main.java
@@ -2,9 +2,10 @@ package HomeWork;
 
 public class Main {
     public static void main(String[] args) {
-        USBank usBank = new USBank(1001,"Ukraine",Curr.EUR,12,1222,3455555);
-        int commission=usBank.getCommission(950);
-        System.out.println("Commission = "+commission);
+        Bank usBank = new USBank(1001,"Ukraine",Curr.EUR,12,1222,3455555);
+        Bank dbank= new USBank(1001,"USA",Curr.USD,25,20001,250000);
+        User user= new User(1166,"Harry",1500.4,25,"Coca-Cola",2000,dbank);
+
 
 
     }
--- src/HomeWork/User.java
+++ src/HomeWork/User.java
@@ -27,14 +27,27 @@ public class User {
      return "name "+name+"\n"+"companyName "+companyName+"\n"+"balance "+balance+"\n"+"salary "+salary+"\n"+"Bank "+bank.getCurrency();
     }
 
+   //public String toString(long id,String name,double balance, int monthsOfEmployment, String companyName,int salary,Bank bank){
+   //   System.out.println("id "+id);
+   //    System.out.println("name "+name);
+   //    System.out.println("balance "+ balance);
+   //    System.out.println("nmonthsOfEmployment "+monthsOfEmployment);
+   //    System.out.println("companyName "+companyName);
+   //    System.out.println("salary "+salary);
+   //    System.out.println("bank "+bank.getBankCountry());
+   //    return companyName;
+
+    //}
+
     // Для примера
 //--------------------------------------------------------------------------------------------------------------------------
 
- //public static void main(String[] args) {
- //    Bank dbank= new USBank(1001,"USA",Curr.USD,25,20001,250000);
- //    User user= new User(1166,"Harry",1500.4,25,"Coca-Cola",2000,dbank);
- //    user.id=1000;
- //    user.name="Петя";
- //    System.out.println(user.toString());
- //}
+ public static void main(String[] args) {
+     Bank dbank= new USBank(1001,"USA",Curr.USD,25,20001,250000);
+     User user= new User(1166,"Harry",1500.4,25,"Coca-Cola",2000,dbank);
+     user.id=1000;
+     user.name="Петя";
+     System.out.println(user.toString());
+     System.out.println(user.id);
+ }
 }
