7c689a3e674a9832c8c94fb50bb6d86909a9c0f6
Denis Azhymov <azhimov93@gmail.com>
2016-11-16 13:54:54 +0200

    init

--- src/HomeWork/ChinaBank.java
+++ src/HomeWork/ChinaBank.java
@@ -1,28 +1,76 @@
 package HomeWork;
 
 public class ChinaBank extends Bank {
+
+    Curr curr = Curr.EUR;
+
     public ChinaBank(long id, String bankCountry, Curr currency, int numberOfEmployee, long rating, long totalCapital) {
         super(id, bankCountry, currency, numberOfEmployee, rating, totalCapital);
     }
 
+
     @Override
     int getLimitOfWithdrawal() {
-        return 0;
+        int LimitOfWithdrawal = 0;
+        switch (curr) {
+            case USD:
+                LimitOfWithdrawal = 100;
+                break;
+            case EUR:
+                LimitOfWithdrawal = 150;
+                break;
+        }
+
+        return LimitOfWithdrawal;
     }
 
     @Override
     int getLimitOfFunding() {
-        return 0;
+        int LimitOfFunding = 0;
+        switch (curr) {
+            case USD:
+                LimitOfFunding = 10000;
+                break;
+            case EUR:
+                LimitOfFunding = 5000;
+        }
+        return LimitOfFunding;
     }
 
     @Override
     int getMonthlyRate() {
-        return 0;
+        int MonthlyRate = 0;
+        switch (curr) {
+            case USD:
+                MonthlyRate = 1;
+                break;
+            case EUR:
+                MonthlyRate = 0;
+                break;
+        }
+        return MonthlyRate;
     }
 
     @Override
     int getCommission(int summ) {
-        return 0;
+        int Commission = 0;
+        switch (curr) {
+            case USD:
+                if (summ < 1000) {
+                    Commission = 3;
+                } else if (summ >= 1000) {
+                    Commission = 5;
+                }
+                break;
+            case EUR:
+                if (summ < 1000) {
+                    Commission = 10;
+                } else if (summ >= 1000) {
+                    Commission = 11;
+                }
+                break;
+        }
+        return Commission;
     }
 
     @Override
